0146d79afa1735965a8c012d84faa8ad356ee39f
JenniD5 <elizabethduran0105@gmail.com>
2021-09-22 16:34:32 -0700

    layout

--- Proyecto1P_Jeyd/ViewController.swift
+++ Proyecto1P_Jeyd/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var playerFondo = AVAudioPlayer()
+    var playerAnimal = AVAudioPlayer()
+    
+    //declaracion de sonindos
+    let urlFondo = Bundle.main.url(forResource: "pollito", withExtension: "mp3")
+    let urlPollito = Bundle.main.url(forResource: "pollo", withExtension: "mp3")
+    
+    @IBOutlet weak var txtNombres: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do{
+            playerFondo = try AVAudioPlayer(contentsOf: urlFondo!, fileTypeHint: AVFileType.mp3.rawValue)
+                
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode:.default)
+            try AVAudioSession.sharedInstance().setActive(true)
+           
+            playerFondo.numberOfLoops = -1
+            playerFondo.volume = 0.2
+            
+            
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        
+        do{
+            playerAnimal = try AVAudioPlayer(contentsOf:urlPollito!, fileTypeHint: AVFileType.mp3.rawValue)
+            playerAnimal.play()
+                   
+                   
+               }catch let error {
+                   print(error.localizedDescription)
+               }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func Tap_pollito(_ sender: Any) {
+        txtNombres.text = "pollito"
+        
+    }
+    
 
 }
 
